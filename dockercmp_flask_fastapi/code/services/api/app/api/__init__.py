457a9c2e33ad5304b162ef70f881aae1d6276282
mrpoopy <junk@krenitsky.org>
2022-11-21 13:53:51 -0500

    added fastapi reload

--- dockercmp_flask_fastapi/code/services/api/app/api/__init__.py
+++ dockercmp_flask_fastapi/code/services/api/app/api/__init__.py
@@ -1,12 +1,26 @@
 """create_app"""
+from typing import Union
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+# class Item(BaseModel):
+#     name: str
+#     price: float
+#     is_offer: Union[bool, None] = None
 
 
 def create_app() -> FastAPI:
+    """factory method for api"""
     app = FastAPI()
 
     @app.get("/")
     def root():
-        return {"message": "Hello World"}
+        return {"message": "Hello Worldz"}
+
+    @app.get("/items/{item_id}")
+    def read_item(item_id: int, q: Union[str, None] = None):
+        return {"item_id": item_id, "q": q}
 
     return app
